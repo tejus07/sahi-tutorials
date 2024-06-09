ac7d11d054ba02dc160c0ad7e441774440ad6143
Tejus Sahi <tejussahi07@gmail.com>
2024-06-09 19:33:24 +0530

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {fileURLToPath, URL} from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/sahi-tutorials/',
+  base: './',
   resolve: {
     alias:{
       "@": fileURLToPath(new URL("./src", import.meta.url)),
