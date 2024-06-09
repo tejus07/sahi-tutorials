e46351e770c8684aef102e566320e00523fdcaa5
Tejus Sahi <tejussahi07@gmail.com>
2024-06-09 15:45:42 +0530

    Changes related to deployment on Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {fileURLToPath, URL} from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/sahi-tutorials/',
   resolve: {
     alias:{
       "@": fileURLToPath(new URL("./src", import.meta.url)),
